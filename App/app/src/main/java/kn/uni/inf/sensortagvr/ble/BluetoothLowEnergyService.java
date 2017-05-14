a91f7fd37ff929e239795877c7cbb60a23faa502
Fabian Klopfer <fabian.klopfer@uni.kn>
2017-05-15 00:45:57 +0200

    Added Intent Request Codes and some parsing impressions

--- App/app/src/main/java/kn/uni/inf/sensortagvr/ble/BluetoothLowEnergyService.java
+++ App/app/src/main/java/kn/uni/inf/sensortagvr/ble/BluetoothLowEnergyService.java
@@ -21,19 +21,43 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import java.util.List;
+import java.util.UUID;
+
 import static kn.uni.inf.sensortagvr.ble.TIUUIDs.*;
+import static kn.uni.inf.sensortagvr.ble.Sensor.*;
 
 public class BluetoothLowEnergyService extends Service {
     private final static String TAG = BluetoothLowEnergyService.class.getSimpleName();
    
     public BluetoothLowEnergyService() {
     }
-
+    // TODO: Queueing & Threading, LocBroadcastListener, Parsing & Profiles
      /*  I/O   */
 
-    /**
+    /*
      *  Intent Codes
      */
+    /* Requests */
+    public final static String ACTION_START_SCAN =
+            "kn.uni.inf.sensortagvr.ble.ACTION_START_SCAN";
+    public final static String ACTION_DEVICE_FOUND =
+            "kn.uni.inf.sensortagvr.ble.ACTION_DEVICE_FOUND";
+    public final static String ACTION_DEVICE_CONNECT =
+            "kn.uni.inf.sensortagvr.ble.ACTION_DEVICE_CONNECT";
+    public final static String ACTION_DEVICE_DISCONNECT =
+            "kn.uni.inf.sensortagvr.ble.ACTION_DEVICE_DISCONNECT";
+    public final static String ACTION_SUBSCRIBE =
+            "kn.uni.inf.sensortagvr.ble.ACTION_GET_DATA";
+    public final static String ACTION_GET_CONFIG =
+            "kn.uni.inf.sensortagvr.ble.ACTION_GET_CONFIG";
+    public final static String WRITE_CONFIG =
+            "kn.uni.inf.sensortagvr.ble.WRITE_CONFIG";
+    public final static String ACTION_CALIBRATE =
+            "kn.uni.inf.sensortagvr.ble.ACTION_CALIBRATE";
+
+    /* Answers */
+    public final static String ACTION_STATE_CHANGED =
+            "kn.uni.inf.sensortagvr.ble.ACTION_STATE_CHANGED";
     public final static String ACTION_GATT_CONNECTED =
             "kn.uni.inf.sensortagvr.ble.ACTION_GATT_CONNECTED";
     public final static String ACTION_GATT_DISCONNECTED =
@@ -42,16 +66,18 @@ public class BluetoothLowEnergyService extends Service {
             "kn.uni.inf.sensortagvr.ble:ACTION_GATT_SERVICES_DISCOVERED";
     public final static String ACTION_DATA_AVAILABLE =
             "kn.uni.inf.sensortagvr.ble.ACTION_DATA_AVAILABLE";
+
+    /* Data Contained in Intent.putExtra() */
+    public final static String EXTRA_SENSOR =
+            "kn.uni.inf.sensortagvr.ble.EXTRA_SENSOR";
     public final static String EXTRA_DATA =
             "kn.uni.inf.sensortagvr.ble.EXTRA_DATA";
     public final static String EXTRA_ADDRESS =
             "kn.uni.inf.sensortagvr.ble.EXTRA_ADDRESS";
     public final static String EXTRA_STATUS =
             "kn.uni.inf.sensortagvr.ble.EXTRA_STATUS";
-    public final static String EXTRA_UUID =
-            "kn.uni.inf.sensortagvr.ble.EXTRA_UUID";
-    public final static String EXTRA_RSSI =
-            "kn.uni.inf.sensortagvr.ble.EXTRA_RSSI";
+
+
 
     LocalBroadcastManager mLocalBroadcastManager =
             LocalBroadcastManager.getInstance(this);
@@ -59,24 +85,34 @@ public class BluetoothLowEnergyService extends Service {
 
 
     private void broadcastUpdate(String action, int rssi, int status) {
-        if (action.equals("EXTRA_RSSI")){
             final Intent intent = new Intent(action);
-            intent.putExtra(EXTRA_RSSI, rssi);
+            intent.putExtra(EXTRA_SENSOR, "RSSI");
+            intent.putExtra(EXTRA_DATA, rssi);
             intent.putExtra(EXTRA_STATUS, status);
             mLocalBroadcastManager.sendBroadcast(intent);
-        }
     }
+
 
     private void broadcastUpdate(String action,
                                  BluetoothGattCharacteristic characteristic,
                                  int status) {
         final Intent intent = new Intent(action);
-        intent.putExtra(EXTRA_UUID, characteristic.getUuid().toString());
-        // TODO Parsing here w switch case
+
+        // TODO Parsing here w switch case which Sensor
+        switch(characteristic.getUuid().toString()) {
+            /* IRT */
+            case "f000aa01-0451-4000-b000-000000000000":
+                intent.putExtra(EXTRA_SENSOR, "IRT");
+                intent.putExtra(EXTRA_DATA, Sensor.convert(characteristic.getValue()));
+                intent.putExtra(EXTRA_STATUS, status);
+                break;
+        }
         intent.putExtra(EXTRA_DATA, characteristic.getValue());
         intent.putExtra(EXTRA_STATUS, status);
         mLocalBroadcastManager.sendBroadcast(intent);
     }
+
+
 
     private void broadcastUpdate(String action, String address, int status) {
         final Intent intent = new Intent(action);
@@ -217,7 +253,7 @@ public class BluetoothLowEnergyService extends Service {
                     for (BluetoothGattCharacteristic ch : mBtGattSvc.getCharacteristics()) {
                         if (ch != null) {
                             gatt.setCharacteristicNotification(ch, true);
-                            BluetoothGattDescriptor descriptor = ch.getDescriptor(UUID_CCC);
+                            BluetoothGattDescriptor descriptor = ch.getDescriptor(UUID.fromString(UUID_CCC));
                             descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
                             gatt.writeDescriptor(descriptor);
                         }

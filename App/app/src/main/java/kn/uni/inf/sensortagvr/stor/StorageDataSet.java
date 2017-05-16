6a5d4d2e1d6e1a5734c471786b4bc598772c6003
Gero Birkhoelzer <gero.birkhoelzer@uni-konstanz.de>
2017-05-16 22:44:09 +0200

    Created BroadcastReceiver, MainService, DataSet class (now with all files)
    Next step is to refine the data flow with th other modules

---
+++ App/app/src/main/java/kn/uni/inf/sensortagvr/stor/StorageDataSet.java
@@ -0,0 +1,56 @@
+package kn.uni.inf.sensortagvr.stor;
+
+
+/**
+ * Basic data type to combine the sensor data with the tracking data
+ * <p>
+ * Created by gero on 16.05.17.
+ */
+
+public class StorageDataSet {
+
+    /* Position */
+    public int xPos;
+    public int yPos;
+    public final int[] position = {xPos, yPos};
+
+    /* Sensor Data, will be expanded */
+    public int sensorData;
+
+    /* Timestamp */
+    public String timestamp;
+
+    /**
+     * Creates a timestamp for every instance of the class
+     */
+    public StorageDataSet() {
+        Long ts = System.currentTimeMillis();
+        this.timestamp = ts.toString();
+    }
+
+    /**
+     * Creates a proper data set to work with
+     *
+     * @param xPos location data
+     * @param yPos location data
+     * @param data sensor data
+     */
+    public StorageDataSet(int xPos, int yPos, int data) {
+        this();
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.sensorData = data;
+    }
+
+    /**
+     * Factory method to create data sets
+     *
+     * @param xPos
+     * @param yPos
+     * @param data
+     * @return
+     */
+    public static StorageDataSet createSet(int xPos, int yPos, int data) {
+        return new StorageDataSet(xPos, yPos, data);
+    }
+}

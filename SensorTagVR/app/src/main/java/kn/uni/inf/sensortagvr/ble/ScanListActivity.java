b68a2289968ecdb953c6cadd60e569d5325998bb
Fabian Klopfer <fabian.klopfer@uni-konstanz.de>
2017-07-03 16:49:31 +0200

    ms2

---
+++ SensorTagVR/app/src/main/java/kn/uni/inf/sensortagvr/ble/ScanListActivity.java
@@ -0,0 +1,202 @@
+package kn.uni.inf.sensortagvr.ble;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kn.uni.inf.sensortagvr.R;
+
+
+/**
+ *
+ */
+public class ScanListActivity extends AppCompatActivity {
+    private static final long SCAN_PERIOD = 5000;
+    static RecyclerView mDeviceList;
+    private final int REQUEST_ENABLE_BT = 1;
+    BluetoothLeScanner mLEScanner;
+    private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler;
+    private boolean mScanning;
+    private ScanSettings settings;
+    private List<ScanFilter> filters;
+    private LeDeviceListAdapter mLeDeviceListAdapter;
+    private final ScanCallback mScanCallback = new ScanCallback() {
+        /**
+         * @param callbackType
+         * @param result
+         */
+        @Override
+        public void onScanResult(int callbackType, final ScanResult result) {
+            runOnUiThread(new Runnable() {
+                /**
+                 *
+                 */
+                @Override
+                public void run() {
+                    mLeDeviceListAdapter.addDevice(result.getDevice());
+                }
+            });
+        }
+
+        /**
+         *
+         * @param results
+         */
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult sr : results) {
+                Log.i("onBatchScanResult", sr.toString());
+            }
+        }
+
+        /**
+         *
+         * @param errorCode
+         */
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e("Scan Failed", "Error Code: " + errorCode);
+        }
+    };
+
+    /**
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar ab = getActionBar();
+        if (ab != null)
+            ab.setTitle(R.string.title_devices);
+
+        // Sets up UI references.
+        mLeDeviceListAdapter = new LeDeviceListAdapter();
+        setContentView(R.layout.activity_scanlist);
+        mDeviceList = (RecyclerView) findViewById(R.id.scanlist);
+        mDeviceList.setAdapter(mLeDeviceListAdapter);
+        mDeviceList.setLayoutManager(new LinearLayoutManager(this));
+
+        mHandler = new Handler();
+
+        // BLE available on this device?
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE Not Supported",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        // Initializes a Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mBluetoothAdapter != null && !(mBluetoothAdapter.isEnabled())) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+        } else {
+            if (Build.VERSION.SDK_INT >= 21 && mBluetoothAdapter != null) {
+                mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+                ScanFilter fi;
+
+                settings = new ScanSettings.Builder()
+                        .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+                        .build();
+
+                fi = new ScanFilter.Builder()
+
+                        .setDeviceName("CC2650 SensorTag")
+                        .build();
+
+                filters = new ArrayList<>();
+                filters.add(fi);
+            }
+
+                scanLeDevice(true);
+        }
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+        mLeDeviceListAdapter.clear();
+    }
+
+    /**
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            //Bluetooth not enabled.
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * @param enable
+     */
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            mHandler.postDelayed(new Runnable() {
+                /**
+                 *
+                 */
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mLEScanner.stopScan(mScanCallback);
+
+                }
+            }, SCAN_PERIOD);
+            mScanning = true;
+            mLEScanner.startScan(filters, settings, mScanCallback);
+        } else {
+            mScanning = false;
+            mLEScanner.stopScan(mScanCallback);
+        }
+        invalidateOptionsMenu();
+    }
+
+
+}
+
+

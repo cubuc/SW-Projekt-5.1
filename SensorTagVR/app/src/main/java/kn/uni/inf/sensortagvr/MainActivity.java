3e333a96975ada7e877fdcba511765ac881d3199
Lisa-Maria <lisamamayer@gmail.com>
2017-07-10 10:43:31 +0200

    Settings-Layout and Record angepasst

--- SensorTagVR/app/src/main/java/kn/uni/inf/sensortagvr/MainActivity.java
+++ SensorTagVR/app/src/main/java/kn/uni/inf/sensortagvr/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         setButton.setOnClickListener(new View.OnClickListener() {
             /**
              * {@inheritDoc}
